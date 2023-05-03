f3e501028ed9feec4444efdfb0d7a2c868e3df0a
Joaquín Elía <joaquin.elia@hotmail.com>
2023-05-03 01:52:21 -0300

    Chat functionality

--- src/app.js
+++ src/app.js
@@ -15,3 +15,18 @@ app.set('view engine', 'handlebars');
 
 const server = app.listen(8080, () => console.log('runnig'));
 const io = new Server(server);
+
+const messages = [];
+
+io.on('connection', socket => {
+    console.log('New user connected');
+    socket.on('message', data => {
+        messages.push(data);
+        io.emit('messageLogs', messages);
+    });
+    
+    socket.on('authenticated', data => {
+        socket.emit('messageLogs', messages);
+        socket.broadcast.emit('userConnected', data);
+    });
+})
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,53 @@
 const socket = io();
+
+// Swal.fire({
+//     title: 'Hola',
+//     text: 'Bienvenido',
+//     icon: 'success'
+// })
+
+let user;
+const chatBox = document.getElementById('chatBox');
+const messageLogs = document.getElementById('messageLogs');
+
+Swal.fire({
+    title: 'Hi to the best chat',
+    input: 'text',
+    text: 'To continue please indentify you',
+    inputValidator: (value) => {
+        return !value && 'You need to enter a user to be able to access'
+    },
+    allowOutsideClick: false
+}).then(result => {
+    user = result.value;
+    socket.emit('authenticated', user);
+});
+
+chatBox.addEventListener('keyup', e => {
+    if(e.key === 'Enter'){
+        if(chatBox.value.trim().length > 0) {
+            socket.emit('message', {user, message: chatBox.value});
+            chatBox.value = '';
+        }
+    }
+})
+
+socket.on('messageLogs', data => {
+    let log = document.getElementById('messageLogs');
+    let messages = [];
+    data.forEach(message => {
+        messages += `${message.user} dice: ${message.message}<br/>`
+    });
+    log.innerHTML = messages;
+});
+
+socket.on('userConnected', data => {
+    Swal.fire({
+        toast: true,
+        position: 'top-end',
+        showConfirmationButton: false,
+        timer: 3000,
+        title: `${data} has joined the chat`,
+        icon: 'success'
+    })
+})
